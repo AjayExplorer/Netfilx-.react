b4c0b760f9074a3dcdc4783b65cda609f622a48d
AjayExplorer <161129605+AjayExplorer@users.noreply.github.com>
2025-01-07 23:42:02 +0530

    make posters and it be scrollable almost be completed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css"
 import NavBar from "./component/NavBar.js/NavBar";
 import Banner from "./component/Banner/Banner";
+import RowPost from "./component/RowPost/RowPost";
 function App() {
   return (
     <div>
       <NavBar/>
       <Banner/>
+      <RowPost/>
     </div>
   );
 }
